c699a1b38f21b441cf92054ccda62901861cebc2
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-05-14 14:47:26 -0300

    Hono.js Setup

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,10 @@
+import { Hono } from "hono"
+import { handle } from "hono/vercel"
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api")
+
+app.get("/hello", (c) => c.json("Hello, World!"))
+export const GET = handle(app)
+export const POST = handle(app)
