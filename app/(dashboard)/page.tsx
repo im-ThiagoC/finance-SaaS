f5321da88ff88b0b8aa56931498a3b88343c02a0
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-05-20 19:39:40 -0300

    Accounts GET API, TanstackQuery added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,17 @@
-import { UserButton } from "@clerk/nextjs";
+"use client"
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
+  const accountsQuery = useGetAccounts();
+
   return (
     <div>
-      Dashboard Page
+      {accountsQuery.data?.map((account) => (
+        <div key={account.id}>
+          {account.name}
+        </div>
+      ))}
     </div>
   );
 }
